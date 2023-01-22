4903c052f3cb7ae41108091f921208ed5233bc7a
Bruno Tomaz <stomaz.bruno@gmail.com>
2023-01-22 14:22:33 -0300

    fix css in SummaryTable

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -13,7 +13,7 @@ export function SummaryTable() {
           return (
             <div
               key={`${weekDay}-${i}`}
-              className="text-zinc-400 text-xl h-10 w-10 font-bold items-center justify-center"
+              className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
             >
               {weekDay}
             </div>
