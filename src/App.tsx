42022add50bd2f55f5f3d148414b29e9150567fe
Bruno Tomaz <stomaz.bruno@gmail.com>
2023-01-22 14:14:56 -0300

    create summaryTable component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 import './styles/global.css'
 
 // import { Habit } from './components/Habit';
@@ -7,8 +8,9 @@ import './styles/global.css'
 export function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <div className="w-full max-w-5xl px-6 flex-col gap-16">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
         <Header />
+        <SummaryTable />
       </div>
     </div>
   )
